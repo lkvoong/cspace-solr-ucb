ed32c8e7b468d6a41a133f51aa6c91ba1d62b887
Lam Voong <lkv@berkeley.edu>
2025-08-27 09:51:18 -0700

    CSW-972: update botgarden/cinefiles solr queries

--- cinefiles/metadata_public.sql
+++ cinefiles/metadata_public.sql
@@ -1,34 +1,37 @@
-select  distinct
-        h1.name as metadata_id,
-        /* coc.id as id2, */
-        coc.numberofobjects,
-        /* coc.computedcurrentlocation, */
-        regexp_replace(coc.collection, '^.*\)''(.*)''$', '\1') AS collection,
-        coc.collection AS collection_refname,
-        coc.distinguishingfeatures,
-        coc.recordstatus,
-        cocf.hasbiblio,
-        regexp_replace(cocf.doctype, '^.*\)''(.*)''$', '\1') AS doctype,
-        cocf.doctype AS doctype_refname,
-        cocf.doctitle,
-        cocf.hasdistco,
-        cocf.doctitlearticle,
-        cocf.hasillust,
-        cocf.hasprodco,
-        cocf.hasfilmog,
-        regexp_replace(cocf.source, '^.*\)''(.*)''$', '\1') AS source,
-        cocf.source as source_refname,
-        cocf.pageinfo,
-        cocf.hascastcr,
-        cocf.hascostinfo,
-        cocf.accesscode,
-        cocf.hastechcr,
-        cocf.docdisplayname,
-        cocf.hasboxinfo,
-        coc.objectnumber AS doc_id,
-        regexp_replace(coc.contentnote, E'[\\t\\n\\r]+', ' ', 'g') AS canonical_url
-from collectionobjects_common coc
-left outer join hierarchy h1 on (h1.id = coc.id)
-left outer join collectionobjects_cinefiles cocf on (coc.id = cocf.id)
-left outer join misc m on (coc.id = m.id and m.lifecyclestate != 'deleted')
-group by h1.id,coc.id,cocf.id;
+select
+  hcc.name as metadata_id,
+  ocg.objectcount as numberofobjects,
+  getdispl(cc.collection) as collection,
+  cc.collection as collection_refname,
+  cc.distinguishingfeatures,
+  cc.recordstatus,
+  cf.hasbiblio,
+  getdispl(cf.doctype) as doctype,
+  cf.doctype as doctype_refname,
+  cf.doctitle,
+  cf.hasdistco,
+  cf.doctitlearticle,
+  cf.hasillust,
+  cf.hasprodco,
+  cf.hasfilmog,
+  getdispl(cf.source) as source,
+  cf.source as source_refname,
+  cf.pageinfo,
+  cf.hascastcr,
+  cf.hascostinfo,
+  cf.accesscode,
+  cf.hastechcr,
+  cf.docdisplayname,
+  cf.hasboxinfo,
+  cc.objectnumber as doc_id,
+  regexp_replace(cc.contentnote, E'[\\t\\n\\r]+', ' ', 'g') as canonical_url
+from collectionobjects_common cc
+join misc mcc on (cc.id = mcc.id and mcc.lifecyclestate != 'deleted')
+join hierarchy hcc on (cc.id = hcc.id)
+left outer join collectionobjects_cinefiles cf on (cc.id = cf.id)
+left outer join hierarchy hocg on (
+  cc.id = hocg.parentid
+  and hocg.pos = 0
+  and hocg.primarytype = 'objectCountGroup')
+left outer join objectcountgroup ocg on (hocg.id = ocg.id)
+;
